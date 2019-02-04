155b1efc5bdb037f792693a7b1ae5e7fbcba6e0f
jevgenijsg <jevgenijs.g@inbox.lv>
2019-02-04 23:43:14 +0200

    #1 added new fields (Category, discount, description)  and validation improvements for price, discount and description.

--- src/com/javaguru/shoppinglist/Product.java
+++ src/com/javaguru/shoppinglist/Product.java
@@ -8,6 +8,9 @@ public class Product {
     private Long id;
     private String name;
     private BigDecimal price;
+    private Category category;
+    private String description;
+    private BigDecimal discount;
 
     public Long getId() {
         return id;
@@ -30,23 +33,58 @@ public class Product {
     }
 
     public void setPrice(BigDecimal price) {
+        if (price.intValue() <= 0) {
+            throw new IllegalArgumentException("Price must be more than zero(0)!!! Please try again");
+        }
         this.price = price;
     }
 
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        if (description.length() < 3 || description.length() > 32) {
+            throw new IllegalArgumentException("Description must be more than 3 and less than 32 characters!!! Please try again");
+        }
+        this.description = description;
+    }
+
+    public BigDecimal getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(BigDecimal discount) {
+        if (discount.doubleValue() > 100) {
+            throw new IllegalArgumentException("Discount can't be more than 100% !!! Please try again");
+        }
+        this.discount = discount;
+    }
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (!(o instanceof Product)) return false;
         Product product = (Product) o;
         return Objects.equals(id, product.id) &&
                 Objects.equals(name, product.name) &&
-                Objects.equals(price, product.price);
+                Objects.equals(price, product.price) &&
+                category == product.category &&
+                Objects.equals(description, product.description) &&
+                Objects.equals(discount, product.discount);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, name, price);
+        return Objects.hash(id, name, price, category, description, discount);
     }
 
     @Override
@@ -55,6 +93,9 @@ public class Product {
                 "id=" + id +
                 ", name='" + name + '\'' +
                 ", price=" + price +
+                ", category=" + category +
+                ", description='" + description + '\'' +
+                ", discount=" + discount +
                 '}';
     }
 }
